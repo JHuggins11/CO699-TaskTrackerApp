91ea895084dc7f3619d661f2f3538e0171f0df59
Jason Huggins <58664223+JHuggins11@users.noreply.github.com>
2023-01-19 23:54:51 +0000

    Add project files.

---
+++ CO699-TaskTrackerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CO699_TaskTrackerApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
