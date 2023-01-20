42714ff38194bdb48e085b6683d475aae7f63e6a
Jason Huggins <58664223+JHuggins11@users.noreply.github.com>
2023-01-20 00:04:49 +0000

    Add User class

---
+++ CO699-TaskTrackerApp/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CO699_TaskTrackerApp.Models
+{
+    /// <summary>
+    /// Represents a user account in the application.
+    /// 
+    /// Created: 19/01/2023
+    /// Modified: 20/01/2023
+    /// </summary>
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DataType(DataType.EmailAddress), MaxLength(50), Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), MinLength(8), MaxLength(40), Required]
+        public string Password { get; set; }
+
+        [Required]
+        public bool Is2FAEnabled { get; set; } = false;
+
+        [DataType(DataType.Date), Required]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
