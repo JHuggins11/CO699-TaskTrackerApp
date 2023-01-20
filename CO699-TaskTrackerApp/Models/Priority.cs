123f9b36667b0f214677211d874cf819a379a9d2
Jason Huggins <58664223+JHuggins11@users.noreply.github.com>
2023-01-20 00:13:58 +0000

    Add Priority enum

---
+++ CO699-TaskTrackerApp/Models/Priority.cs
@@ -0,0 +1,15 @@
+﻿namespace CO699_TaskTrackerApp.Models
+{
+    /// <summary>
+    /// Contains the priority levels for a task.
+    /// 
+    /// Created: 20/01/2023
+    /// Modified: 20/01/2023
+    /// </summary>
+    public enum Priority
+    {
+        High, 
+        Moderate, 
+        Low
+    }
+}
