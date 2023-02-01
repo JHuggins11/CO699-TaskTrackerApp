a38f796a347e85a71942795ef75276e920a193ca
Jason Huggins <jasonhuggins11@outlook.com>
2023-02-01 22:21:44 +0000

    Check current user ID and task's user ID match

--- CO699-TaskTrackerApp/Pages/UserTasks/Index.cshtml.cs
+++ CO699-TaskTrackerApp/Pages/UserTasks/Index.cshtml.cs
@@ -8,28 +8,41 @@ using Microsoft.EntityFrameworkCore;
 using CO699_TaskTrackerApp.Data;
 using CO699_TaskTrackerApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace CO699_TaskTrackerApp.Pages.UserTasks
 {
-    [Authorize]
+    //[Authorize]
     public class IndexModel : PageModel
     {
         private readonly CO699_TaskTrackerApp.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager; 
 
-        public IndexModel(CO699_TaskTrackerApp.Data.ApplicationDbContext context)
+        public IndexModel(CO699_TaskTrackerApp.Data.ApplicationDbContext context, 
+            UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<UserTask> UserTask { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
+            // Only display tasks linked to the current user's ID
+            var userTasks = from u in _context.UserTask
+                            select u;
+            var currentUserId = _userManager.GetUserId(User);
+
             if (_context.UserTask != null)
             {
-                UserTask = await _context.UserTask
-                .Include(u => u.User).ToListAsync();
+                userTasks = userTasks.Where(u => u.UserId.ToString() == currentUserId);
+
+                //UserTask = await _context.UserTask
+                //.Include(u => u.User).ToListAsync();
             }
+
+            UserTask = await userTasks.ToListAsync();
         }
     }
 }
