d15cce40c4aba1ba40572068556a1b36028dd9ee
Jason Huggins <jasonhuggins11@outlook.com>
2023-02-15 23:25:33 +0000

    Start DbInitialiser class for database seeding

---
+++ CO699-TaskTrackerApp/Data/DbInitialiser.cs
@@ -0,0 +1,38 @@
+﻿using CO699_TaskTrackerApp.Models;
+
+namespace CO699_TaskTrackerApp.Data
+{
+    /// <summary>
+    /// Test data used to seed the database with sample users, user accounts and tasks.
+    /// 
+    /// Created: 15/02/2023
+    /// Modified: 15/02/2023
+    /// </summary>
+    public class DbInitialiser
+    {
+        public static void Initialise(ApplicationDbContext context)
+        {
+            // TODO: Add method calls
+
+        }
+
+        private static void AddUsers(ApplicationDbContext context)
+        {
+            // Skip database seeding if any User records are found
+            if (context.Users.Any())
+            {
+                return; 
+            }
+
+            // TODO: Continue
+            var users = new User[]
+            {
+                new User
+                {
+                    Email = "user1@email.com",
+                    
+                }
+            };
+        }
+    }
+}
