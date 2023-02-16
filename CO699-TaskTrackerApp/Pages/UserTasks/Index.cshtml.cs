6cc1cdee65fbabe0a5b7bb9e2e3d9d7098c3f61d
Jason Huggins <jasonhuggins11@outlook.com>
2023-02-16 22:40:13 +0000

    Add code from previous scaffold

--- CO699-TaskTrackerApp/Pages/UserTasks/Index.cshtml.cs
+++ CO699-TaskTrackerApp/Pages/UserTasks/Index.cshtml.cs
@@ -7,26 +7,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CO699_TaskTrackerApp.Data;
 using CO699_TaskTrackerApp.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace CO699_TaskTrackerApp.Pages.UserTasks
 {
     public class IndexModel : PageModel
     {
         private readonly CO699_TaskTrackerApp.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(CO699_TaskTrackerApp.Data.ApplicationDbContext context)
+        public IndexModel(CO699_TaskTrackerApp.Data.ApplicationDbContext context,
+            UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<UserTask> UserTask { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
+            // Only display tasks linked to the current user's ID
+            var userTasks = from u in _context.UserTask
+                            select u;
+            var currentUserId = _userManager.GetUserId(User);
+
             if (_context.UserTask != null)
             {
-                UserTask = await _context.UserTask.ToListAsync();
+                userTasks = userTasks.Where(u => u.UserID == currentUserId);
             }
+
+            UserTask = await userTasks.ToListAsync();
         }
     }
 }
