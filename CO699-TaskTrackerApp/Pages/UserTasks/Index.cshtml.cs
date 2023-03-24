7a382ce70d779c813726979b65ad957a4010b1de
Jason Huggins <jasonhuggins11@outlook.com>
2023-03-24 19:14:05 +0000

    Add sorting code

--- CO699-TaskTrackerApp/Pages/UserTasks/Index.cshtml.cs
+++ CO699-TaskTrackerApp/Pages/UserTasks/Index.cshtml.cs
@@ -23,21 +23,49 @@ namespace CO699_TaskTrackerApp.Pages.UserTasks
             _userManager = userManager;
         }
 
-        public IList<UserTask> UserTask { get;set; } = default!;
+        public string DateSort { get; set; }
+        public string PrioritySort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<UserTask> UserTasks { get;set; } = default!;
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            // Only display tasks linked to the current user's ID
-            var userTasks = from u in _context.UserTask
-                            select u;
-            var currentUserId = _userManager.GetUserId(User);
+            // Reference: https://learn.microsoft.com/en-gb/aspnet/core/data/ef-rp/sort-filter-page?view=aspnetcore-7.0
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            PrioritySort = sortOrder == "Priority" ? "priority_desc" : "Priority";
 
-            if (_context.UserTask != null)
+            IQueryable<UserTask> tasksIQ = from t in _context.UserTask
+                                           select t;
+
+            switch (sortOrder)
             {
-                userTasks = userTasks.Where(u => u.UserID == currentUserId);
+                case "date_desc":
+                    tasksIQ = tasksIQ.OrderByDescending(t => t.DueDate);
+                    break;
+                case "Priority":
+                    tasksIQ = tasksIQ.OrderBy(t => t.Priority);
+                    break;
+                case "priority_desc":
+                    tasksIQ = tasksIQ.OrderByDescending(t => t.Priority);
+                    break;
+                default:
+                    tasksIQ = tasksIQ.OrderBy(t => t.DueDate);
+                    break;
             }
 
-            UserTask = await userTasks.ToListAsync();
+            //// Only display tasks linked to the current user's ID
+            //var userTasks = from u in _context.UserTask
+            //                select u;
+            //var currentUserId = _userManager.GetUserId(User);
+
+            //if (_context.UserTask != null)
+            //{
+            //    userTasks = userTasks.Where(u => u.UserID == currentUserId);
+            //}
+
+            UserTasks = await tasksIQ.AsNoTracking().ToListAsync();
         }
     }
 }
