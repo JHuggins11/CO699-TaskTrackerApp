faaa803c9f8926dcdb3ec8bbaed740e36e696547
Jason Huggins <jasonhuggins11@outlook.com>
2023-03-24 19:53:47 +0000

    Add sample tasks for database seeding

--- CO699-TaskTrackerApp/Data/DbInitialiser.cs
+++ CO699-TaskTrackerApp/Data/DbInitialiser.cs
@@ -6,19 +6,55 @@ namespace CO699_TaskTrackerApp.Data
     /// Test data used to seed the database with sample tasks.
     /// 
     /// Created: 15/02/2023
-    /// Modified: 16/02/2023
+    /// Modified: 24/03/2023
     /// </summary>
     public class DbInitialiser
     {
         public static void Initialise(ApplicationDbContext context)
         {
-            // TODO: Add method calls
-
+            AddUserTasks(context);
         }
 
         private static void AddUserTasks(ApplicationDbContext context)
         {
-            // TODO: Add sample tasks
+            // Skip seeding if existing tasks in the database are found
+            if (context.UserTask.Any())
+            {
+                return;
+            }
+
+            // Add sample tasks
+            var userTasks = new UserTask[]
+            {
+                new UserTask
+                {
+                    // UserID = 
+                    Name = "Complete homework",
+                    Priority = Priority.Moderate,
+                    DueDate = DateTime.Parse("2023-02-15"),
+                    Description = "See assignment brief for more details."
+                },
+
+                new UserTask
+                {
+                    // UserID = 
+                    Name = "Revise for exam",
+                    Priority = Priority.High,
+                    DueDate = DateTime.Parse("2023-03-24"),
+                    Description = "Review past exam papers for practice."
+                },
+
+                new UserTask
+                {
+                    // UserID = 
+                    Name = "Go shopping",
+                    Priority = Priority.Low,
+                    DueDate = DateTime.Parse("2023-03-26")
+                }
+            };
+
+            context.UserTask.AddRange(userTasks);
+            context.SaveChanges();
         }
     }
 }
