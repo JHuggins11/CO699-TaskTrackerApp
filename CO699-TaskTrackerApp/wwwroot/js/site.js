2e95d5a9d7694d0a6ab7a1344f32ddce8bed44bc
Jason Huggins <jasonhuggins11@outlook.com>
2023-04-03 16:55:56 +0100

    Add code to strikethrough task's text when marked as complete

--- CO699-TaskTrackerApp/wwwroot/js/site.js
+++ CO699-TaskTrackerApp/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// UserTasks/Index.cshtml
+
+var taskName = document.getElementById("task-name");
+var taskDueDate = document.getElementById("task-due-date");
+var taskPriority = document.getElementById("task-priority");
+var taskDescription = document.getElementById("task-description");
+
+// Strikethrough task's text in Bootstrap card if the task has been set as completed
+function strikeText() {
+    taskName.classList.add("completed-task");
+    taskDueDate.classList.add("completed-task");
+    taskPriority.classList.add("completed-task");
+    taskDescription.classList.add("completed-task");
+}
+
+// Remove strikethrough text from task's Bootstrap card if the task has been set as incomplete
+function removeStrikeText() {
+    taskName.classList.remove("completed-task");
+    taskDueDate.classList.remove("completed-task");
+    taskPriority.classList.remove("completed-task");
+    taskDescription.classList.remove("completed-task");
+}
